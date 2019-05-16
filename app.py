4889779eba25c05d0c1f17f7e7cc18961b6ca4ad
namanlalit <naman.lalit@gmail.com>
2019-05-16 23:52:43 +0530

    Modified Work

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask,render_template, redirect, url_for, request, make_response,pandas
-import requests as rq
+from flask import Flask,render_template, redirect, url_for, request, make_response
 app = Flask(__name__)
 
 array = []
-@app.route('/', methods = ['POST', 'GET'])
-def details():
-    name = request.form['FullName']
-    array.append(name)
-    return render_template('error.html')
 
-@app.route('/data', methods = ['POST'])
+@app.route("/")
+def details():
+    return render_template('login.html')
+
+@app.route("/data", methods = ['POST','GET'])
 def data():
-   render_template('data.html') 
+    return render_template("data.html",len = len(array), array=array)
+
+@app.route("/error",methods = ['POST' , 'GET'])
+def error():
+    if request.method=='POST':
+        value = request.form['Name']
+        print(value)
+        if(value not in array):
+            array.append(value)
+        print(array)
+    return render_template("error.html")
+if __name__=='__main__':
+	app.run(debug = True)
